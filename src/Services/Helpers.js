ced6eeb807bb68c9e0ed4e4fb341352dc2d42ade
catalinasy <catalinasyddall@gmail.com>
2019-05-01 14:10:42 -0300

    adds localStorage and apiCalls helpers

--- src/Services/Helpers.js
+++ src/Services/Helpers.js
@@ -12,3 +12,24 @@ export function formatCharacters(characters) {
     };
   });
 }
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+export function setLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+export async function getCharactersFromAPI() {
+  const data = await apiCall("http://swapi.co/api/people");
+  const rawCharacters = data.results;
+  return formatCharacters(rawCharacters);
+}
+export function getFilmsByCharacter(character) {
+  return Promise.all(
+    character.films.map(f => {
+      return apiCall(f);
+    })
+  );
+}
+export function verifyCachedFilmsByCharacter(character) {
+  return getLocalStorage(`starwars-films-${character.id}`);
+}
