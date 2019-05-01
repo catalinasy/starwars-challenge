a831945605e2eeca310bf8b6b32c90f071ff107d
catalinasy <catalinasyddall@gmail.com>
2019-05-01 14:02:46 -0300

    imports functions from helpers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import SearchBarContainer from "./Containers/SearchBarContainer";
-import { apiCall, formatCharacters } from "./Services/Helpers";
-import List from "./Components/List"
+import { List } from "./Components/List";
+import SearchBar from "./Components/SearchBar";
+import { NotFound } from "./Components/NotFound";
+import { Loading } from "./Components/Loading/Loading";
+import {
+  getLocalStorage,
+  getCharactersFromAPI,
+  setLocalStorage,
+  getFilmsByCharacter,
+  verifyCachedFilmsByCharacter
+} from "./Services/helpers";
 
 
 export default class App extends Component {
